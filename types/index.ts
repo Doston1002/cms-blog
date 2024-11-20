d78aa373e2a12dc589e3d1218338f07a832c8fe0
Doston1002 <polatovdoston1002@gmail.com>
2024-11-20 13:08:41 +0500

    blog page

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,10 @@
 export interface ChildProps {
 	children: React.ReactNode
 }
+export interface IBlog{
+    title:string
+    description:string
+    author:string
+    image:string
+    tags:string[]
+}
