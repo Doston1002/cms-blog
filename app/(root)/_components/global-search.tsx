b4fec3043b02666354f6b91859a0102131ad7a48
Doston1002 <polatovdoston1002@gmail.com>
2024-11-21 11:33:09 +0500

    search category & tag

--- app/(root)/_components/global-search.tsx
+++ app/(root)/_components/global-search.tsx
@@ -1,8 +1,9 @@
 import { Badge } from '@/components/ui/badge'
-import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
+import { Drawer, DrawerClose, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
 import { Input } from '@/components/ui/input'
 import { popularCategories, popularTags } from '@/constants'
-import { Search } from 'lucide-react'
+import { Minus, Search } from 'lucide-react'
+import Link from 'next/link'
 
 function GlobalSearch() {
 	return (
@@ -21,7 +22,17 @@ function GlobalSearch() {
 					/>
 
 					<div className='flex flex-col space-y-2 mt-4'>
+						<div className='flex items-center gap-2'>
+
 						<p className='font-creteRound text-2xl'>See posts by categories</p>
+						<Minus />
+
+						<Link href={'/categories'} className='text-blue-500 underline hover:opacity-90'>
+						<DrawerClose>
+							See all
+						</DrawerClose>
+						</Link>
+						</div>
 						<div className='flex flex-wrap gap-2'>
 							{popularCategories.map(item => (
 								<Badge key={item.slug} variant={'secondary'}>
@@ -32,7 +43,15 @@ function GlobalSearch() {
 					</div>
 
 					<div className='flex flex-col space-y-2 mt-4'>
+						<div className='flex items-center gap-2'>
 						<p className='font-creteRound text-2xl'>See posts by tags</p>
+						<Minus />
+						<Link href={'/tags'} className='text-blue-500 underline hover:opacity-90'>
+						<DrawerClose>
+							See all
+						</DrawerClose>
+						</Link>
+						</div>
 						<div className='flex flex-wrap gap-2'>
 							{popularTags.map(item => (
 								<Badge key={item.slug} variant={'secondary'}>
